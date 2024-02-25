7040919466b91fbcddbf1c84527667a4df71d25d
Jerry John Thomas <68646979+JerryJohnThomas@users.noreply.github.com>
2024-02-25 18:19:09 +0530

    Ep1 completed

--- src/CommonPages/Home.js
+++ src/CommonPages/Home.js
@@ -1,20 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Home.css";
 function Home() {
     return (
         <div className="HomeContainer">
             <h1>Three JS Medium Series</h1>
-            Full Series - here  
-
+            Full Series - here
             <h3> Season 1</h3>
-
-            <Link to="/s1e1">Episode 1 </Link>
-
+            <Link to="/s1e1">Episode 1 - Mesh </Link>
             <br />
             <br />
             <br />
-
             <div> PS: I know that this looks ugly but this works for now</div>
         </div>
     );
--- src/Episodes_S1/E1.js
+++ src/Episodes_S1/E1.js
@@ -1,13 +1,29 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+function Cube() {
+    const MeshRef = useRef();
+
+    useFrame(() => {
+        if (!MeshRef.current) return;
+        MeshRef.current.rotation.x += 0.01;
+        MeshRef.current.rotation.y += 0.01;
+    });
+
+    return (
+        <mesh ref={MeshRef}>
+            <boxGeometry args={[2, 2, 2]} />
+            <meshStandardMaterial color="hotpink" />
+        </mesh>
+    );
+}
 
 function E1() {
     return (
         <Canvas>
-            <mesh>
-                <boxGeometry />
-                <meshStandardMaterial />
-            </mesh>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Cube />
         </Canvas>
     );
 }
